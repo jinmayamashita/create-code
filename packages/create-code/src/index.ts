0d87194f59305ff1cdf4135c544ba1cae2a97b61
Jinma Yamashita <9401060+jinmayamashita@users.noreply.github.com>
2023-01-12 12:04:41 +0900

    copy preview app

--- packages/create-code/src/index.ts
+++ packages/create-code/src/index.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 
+import * as path from "node:path";
+import fse from "fs-extra";
+import Enquirer from "enquirer";
 import gradient from "gradient-string";
 import chalk from "chalk";
+import meow from "meow";
 
 // helpers
 const welcome = async (title: string, desc: string) => {
@@ -10,15 +14,71 @@ const welcome = async (title: string, desc: string) => {
   console.log(chalk.hex("#0052D4")(desc));
 };
 
+const help = `
+  Usage:
+    $ npx create-code [flags...] [<dir>]
+  If <dir> is not provided up front you will be prompted for it.
+  Flags:
+    --help, -h          Show this help message
+    --version, -v       Show the version of this script
+`;
+
 async function run() {
+  const { input, flags } = meow(help, {
+    flags: {
+      help: { type: "boolean", default: false, alias: "h" },
+      mini: { type: "boolean", default: false, alias: "m" },
+      library: { type: "string", default: "react", alias: "lib" },
+      version: { type: "boolean", default: false, alias: "v" },
+    },
+  });
+
   await welcome(
     "\n≛ Create code\n",
-    "Let's quickly start a project with create code!\n"
+    "Let's quickly start a development app with some base code!\n"
   );
+
+  const appDir = path.resolve(
+    process.cwd(),
+    input.length > 0
+      ? input[0]
+      : (
+          await Enquirer.prompt<{ dir: string }>({
+            type: "input",
+            name: "dir",
+            initial: "./my-app",
+            message: "Where would you like to create your app?",
+          })
+        ).dir
+  );
+
+  const appName = path.basename(appDir);
+  fse.mkdirSync(appDir, { recursive: true });
+
+  // Copy preview app
+  const appLibrary = "react-app";
+  const previewAppDir = path.resolve("../../..", "preview", appLibrary);
+
+  fse.copySync(previewAppDir, appDir, {
+    filter: (src) => !["node_modules"].includes(path.basename(src)),
+  });
+
+  return appName;
 }
 
-async function success() {
-  console.log("success");
+async function success(appDir: string) {
+  console.log(
+    `${chalk.bold(
+      gradient(["#0052D4", "#65C7F7", "#9CECFB"])("Success!")
+    )} Created a new app at "${appDir}".`
+  );
+  console.log("Inside this directory, you can run several commands:");
+  console.log();
+  console.log(chalk.cyan(`  pnpm dev`));
+  console.log(`     Develop your app`);
+  console.log();
+  console.log(chalk.cyan(`  pnpm build`));
+  console.log(`     Build your app`);
   process.exit();
 }
 
