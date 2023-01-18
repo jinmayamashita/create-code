c8f1b2cc60bc4689461c9098512fc94b2b3ae396
Jinma Yamashita <9401060+jinmayamashita@users.noreply.github.com>
2023-01-18 10:32:40 +0900

    add navigation to home page for now

--- examples/react-standard/src/pages/home.tsx
+++ examples/react-standard/src/pages/home.tsx
@@ -1,5 +1,27 @@
+import { Link } from "wouter";
 import { ThemeToggle } from "../components/theme-toggle";
 
+function PageNavigation() {
+  return (
+    <nav
+      style={{
+        position: "fixed",
+        display: "flex",
+        width: 300,
+        justifyContent: "space-around",
+      }}
+    >
+      <Link to="/toggle">Toggle example</Link>
+      <Link to="password">Password example</Link>
+    </nav>
+  );
+}
+
 export default function Home() {
-  return <ThemeToggle />;
+  return (
+    <>
+      <PageNavigation />
+      <ThemeToggle />
+    </>
+  );
 }
